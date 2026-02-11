1cd9abd0d6d5d7c3a232f16af02769e3b7c316db
AMIT KUMAR <162543776+amit-zamplia@users.noreply.github.com>
2026-02-11 16:47:07 +0530

    remove test data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 import express from "express";
-import testController from "../controller/testController.js";
 import businessEnquiryController from "../controller/businessEnquiryController.js";
-import transporter from "../utils/mailer.js";
 import upload from "../middleware/upload.js";
 import jobController from "../controller/jobController.js";
 
 const router = express.Router();
 
-router.get("/test", testController.test);
 router.post("/business-inquiry", businessEnquiryController.enquiry);
 router.post("/job", upload.fields([
     { name: "resume", maxCount: 1 },
@@ -15,31 +12,6 @@ router.post("/job", upload.fields([
 ]),
     jobController.submitApplication
 );
-
-router.get("/mail-test", async (req, res) => {
-    try {
-        await transporter.sendMail({
-            from: 'shekeamit@gmail.com',
-            to: 'amit.kumar@zamplia.com',
-            subject: "Test Email",
-            text: "Email working!",
-        });
-
-        return res.status(200).json({
-            success: true,
-            message: "Mail sent successfully ✅",
-        });
-
-    } catch (error) {
-        console.error("Mail Test Error:", error);
-
-        return res.status(500).json({
-            success: false,
-            message: "Failed to send email ❌",
-            error: error.message,
-        });
-    }
-});
 
 
 export default router;
